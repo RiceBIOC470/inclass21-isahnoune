e10ff0af874c4514afb6dc9f83349a8615b3ee64
isahnoune <31284645+isahnoune@users.noreply.github.com>
2017-11-16 14:47:44 -0600

    Add files via upload

--- Inclass21.m
+++ Inclass21.m
@@ -8,9 +8,51 @@
 % out off ( x(0) = 0 and y(0) = 0). Plot the value of x and y in time and 
 % explain your results. 
 
+%dx = k-d*x, dy = k2*x-d2*y
+%k = 1, d = 2, k2 = 3, d2 = 4
+
+F = @(t,x) [1-2*x(1);3*x(1) - 4*x(2)]; 
+Fresults = ode23(F, [0 100],[0,0]);
+plot(Fresults.x, Fresults.y(1,:)); 
+hold on;
+plot(Fresults.x, Fresults.y(2,:)); 
+hold off;
+
+%The plot peaks quickly and then plateaus at 0.35 and 0.5, implying
+%degradation at approximately t = 2 to t = 4.
+
+
 % 2. Consider the following model: 
 % dx/dt = y
 % dy/dt = (1-x^2)*y - x
 % write code that simulates this system for several different starting
 % values of x and y. Plot the results and comment on them. 
 
+F = @(t,x) [x(2);(1-x(1)^2)*x(2) - x(1)]; 
+
+xstart = rand();
+ystart = rand();
+Fresults = ode23(F, [0 10],[xstart,ystart]);
+figure;
+plot(sol.x, sol.y(1,:));
+hold on;
+plot(sol.x, sol.y(2,:));
+ 
+xstart = rand();
+ystart = rand();
+Fresults = ode23(F, [0 10],[xstart,ystart]);
+figure;
+plot(sol.x, sol.y(1,:));
+hold on;
+plot(sol.x, sol.y(2,:));
+
+xstart = rand();
+ystart = rand();
+Fresults = ode23(F, [0 10],[xstart,ystart]);
+figure;
+plot(sol.x, sol.y(1,:));
+hold on;
+plot(sol.x, sol.y(2,:));
+
+%There are variable results according to whether x is greater than y and
+%vice versa.
